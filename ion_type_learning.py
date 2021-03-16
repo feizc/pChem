12ba4769bdfe95c102a0c6fd57d4eaafa480cfd3
费政聪 <3300949577@qq.com>
2021-03-16 22:27:20 +0800

    Add files via upload

--- ion_type_learning.py
+++ ion_type_learning.py
@@ -3,10 +3,27 @@
 from utils import parameter_file_read 
 import os 
 
+class MassSpectrum:
+    def __init__(self, charge, pepmass, peak_list):
+        self.charge = charge 
+        self.pepmass = pepmass
+        self.peak_list = peak_list 
+
+
 def ion_type_determine(current_path): 
     pchem_cfg_path = os.path.join(current_path, 'pChem.cfg')
     parameter_dict = parameter_file_read(pchem_cfg_path) 
-    print(parameter_dict)
+    
+    mass_spectra_dict = {}
+    # 数据读取 
+    for msms_path in parameter_dict['msms_path']:
+        print(msms_path)
+    
+
+
+# 从mgf文件中读取谱图
+def mgf_read():
+    print('1')
 
 
 if __name__ == "__main__": 
